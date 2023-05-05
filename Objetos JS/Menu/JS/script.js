5888600d566660d67dfa532c6e747205eed7374b
Pedro Paulo Ramalho de Almeida <108875939+P3dr0p@users.noreply.github.com>
2023-05-05 12:29:57 -0300

    Add files via upload

--- Objetos JS/Menu/JS/script.js
+++ Objetos JS/Menu/JS/script.js
@@ -34,7 +34,6 @@ const menu = [
         image: "./images/item-4.jpeg",
         description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch.Prism austin mlkshk truffaut.",
     },
-
     {
         id: 5,
         title: "Egg Attack",
@@ -43,7 +42,6 @@ const menu = [
         image: "./images/item-5.jpeg",
         description: "Franzen vegan pabst bicycle rights kickstater pinterest meditation farm-to-table 90's pop-up",
     },
-
     {
         id: 6,
         title: "Oreo Dream",
@@ -52,7 +50,6 @@ const menu = [
         image: "./images/item-6.jpeg",
         description: "Portland chicharrones ethical edison, palo santo craft beer chia heirloom iphone everyday",
     },
-    
     {
         id: 7,
         title: "Bacon Overflow",
@@ -61,13 +58,28 @@ const menu = [
         image: "./images/item-7.jpeg",
         description: "Carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird",
     },
-
     {
         id: 8,
         title: "American Classic",
         category: "Lunch",
         price: "12.99",
         image: "./images/item-8.jpeg",
-        description: "Carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird",
+        description: "On it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings Food truck truffaut",
+    },
+    {
+        id: 9,
+        title: "Quarantine Buddy",
+        category: "Shakes",
+        price: "16.99",
+        image: "./images/item-9.jpeg",
+        description: "Skateboarder farn synth authentic semiotics. Live-edge lyft af, edilson buld yuccie crucifx microdosing.",
+    },
+    {
+        id: 10,
+        title: "Stack Dinner",
+        category: "Dinner",
+        price: "39.99",
+        image: "./images/item-10.jpeg",
+        description: "Skateboarder farn synth authentic semiotics. Live-edge lyft af, edilson buld yuccie crucifx microdosing.",
     },
 ];
