02fae153d3b008922f7ed8feeb76ba13f29f28f7
Pedro Paulo Ramalho de Almeida <108875939+P3dr0p@users.noreply.github.com>
2023-05-08 12:21:28 -0300

    Add files via upload

--- Objetos JS/Menu/JS/script.js
+++ Objetos JS/Menu/JS/script.js
@@ -4,34 +4,31 @@ const menu = [
         title: "Buttermilk Pancakes",
         category: "Breakfast",
         price: 15.99,
-        image: "./images/item-1.jpeg",
+        image: "./IMG/item-1.jpeg",
         description: "I'm baby woke milkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed."
     },
-
     {
         id: 2,
         title: "Diner Double",
         category: "Lunch",
         price: 13.99,
-        image: "./images/item-2.jpeg",
+        image: "./IMG/item-2.jpeg",
         description: "Vaporware iphone mumnlecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing."
     },
-
     {
         id: 3,
         title: "Godzilla Milkshake",
         category: "Shakes",
         price: "6.99",
-        image: "./images/item-3.jpeg",
+        image: "./IMG/item-3.jpeg",
         description: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold organic viral",
     },
-
     {
         id: 4,
         title: "Country Delight",
         category: "Breakfast",
         price: "20.99",
-        image: "./images/item-4.jpeg",
+        image: "./IMG/item-4.jpeg",
         description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch.Prism austin mlkshk truffaut.",
     },
     {
@@ -39,7 +36,7 @@ const menu = [
         title: "Egg Attack",
         category: "Lunch",
         price: "22.99",
-        image: "./images/item-5.jpeg",
+        image: "./IMG/item-5.jpeg",
         description: "Franzen vegan pabst bicycle rights kickstater pinterest meditation farm-to-table 90's pop-up",
     },
     {
@@ -47,7 +44,7 @@ const menu = [
         title: "Oreo Dream",
         category: "Shekes",
         price: "18.99",
-        image: "./images/item-6.jpeg",
+        image: "./IMG/item-6.jpeg",
         description: "Portland chicharrones ethical edison, palo santo craft beer chia heirloom iphone everyday",
     },
     {
@@ -55,7 +52,7 @@ const menu = [
         title: "Bacon Overflow",
         category: "Breakfast",
         price: "8.99",
-        image: "./images/item-7.jpeg",
+        image: "./IMG/item-7.jpeg",
         description: "Carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird",
     },
     {
@@ -63,7 +60,7 @@ const menu = [
         title: "American Classic",
         category: "Lunch",
         price: "12.99",
-        image: "./images/item-8.jpeg",
+        image: "./IMG/item-8.jpeg",
         description: "On it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings Food truck truffaut",
     },
     {
@@ -71,7 +68,7 @@ const menu = [
         title: "Quarantine Buddy",
         category: "Shakes",
         price: "16.99",
-        image: "./images/item-9.jpeg",
+        image: "./IMG/item-9.jpeg",
         description: "Skateboarder farn synth authentic semiotics. Live-edge lyft af, edilson buld yuccie crucifx microdosing.",
     },
     {
@@ -79,7 +76,58 @@ const menu = [
         title: "Stack Dinner",
         category: "Dinner",
         price: "39.99",
-        image: "./images/item-10.jpeg",
-        description: "Skateboarder farn synth authentic semiotics. Live-edge lyft af, edilson buld yuccie crucifx microdosing.",
+        image: "./IMG/item-10.jpeg",
+        description: "Skateboard fam synth authentic semiotics. Live-edge lyft af, edison buld yuccie crucifix microdosing",
     },
 ];
+
+const menuItems = document.querySelector('.menu-items');
+const buttonContainer = document.querySelector('.button-container');
+
+window.addEventListener("DOMContentLoaded", () => {
+    displayMenuButtons()
+});
+
+
+menu.map((item) => {
+    let html = `
+    <div class="content">
+    <div class="item-image">
+        <img src="${item.image}" alt="${item.title}" />
+    </div>
+
+    <div class="item-info">
+    <div class="item-title">
+        <p>${item.title}</p>
+        <p class="price">${item.price}</p>
+    </div>
+
+    <div class="item-description">
+        <p>
+        ${item.description}
+        </p>
+       </div>
+    </div>
+  </div> 
+  `
+ menuItems.innerHTML += html
+});
+
+
+function displayMenuButtons() {
+    const categories = menu.reduce((value, item) => {
+        if(!value.includes(item.category)) {
+            value.push(item.category)
+        }
+    
+        return value;
+    }, ["all"]);
+
+    const categoryBtns = categories.map((category) => {
+        return `
+        <button class="filter-button" data-id="${category}">${category}</button>
+        `
+    }).join("");
+
+    buttonContainer.innerHTML += categoryBtns;
+}
